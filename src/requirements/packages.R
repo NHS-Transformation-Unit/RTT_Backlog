eed846d61168e13cfa580ed5ad9204ba8f033c0c
ASW-Analyst <andy.wilson8@nhs.net>
2024-01-15 12:01:43 +0000

    Add packages.R to requirements

---
+++ src/requirements/packages.R
@@ -0,0 +1,8 @@
+packages <- c("here",
+              "tidyverse",
+              "readxl",
+              "leaflet",
+              "markdown",
+              "knitr")
+
+lapply(packages, library, character.only=TRUE)
