fa0af32e0f18eae705e45e2d8ca0da12b2ec80ff
ASW-Analyst <andy.wilson8@nhs.net>
2024-01-16 10:20:39 +0000

    Add load for weeks breakdown

--- src/processing/load.R
+++ src/processing/load.R
@@ -1,4 +1,5 @@
 
 # Load Total RTT Backlog --------------------------------------------------
 
-rtt_total <- read_excel(paste0(here(),"/data/rtt_total.xlsx"))
+rtt_total <- read_excel(paste0(here(), "/data/rtt_total.xlsx"))
+rtt_total_weeks <- read_excel(paste0(here(), "/data/rtt_total_weeks.xlsx"))
