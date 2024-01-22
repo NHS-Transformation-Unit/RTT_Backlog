5c2105336d82810f6db5366095e7111eccd3ea2b
ASW-Analyst <andy.wilson8@nhs.net>
2024-01-22 23:03:00 +0000

    Formatting of long waits chart as line chart

--- src/visualisation/long_waits.R
+++ src/visualisation/long_waits.R
@@ -24,10 +24,14 @@ rtt_total_long_waits_chart <- ggplot(rtt_total_long_waiters, aes(x = as.Date(Eff
 rtt_total_long_waits_chart
 
 rtt_total_long_waiters_post <- rtt_total_long_waiters %>%
-  filter(Effective_Snapshot_Date > '2021-03-31')
+  filter(Effective_Snapshot_Date > '2021-05-31')
+
+rtt_total_long_waiters_post_recent <- rtt_total_long_waiters_post %>%
+  filter(Effective_Snapshot_Date == max(Effective_Snapshot_Date))
 
 rtt_total_long_waits_chart_post <- ggplot(rtt_total_long_waiters_post, aes(x = as.Date(Effective_Snapshot_Date), y = Count, fill = Group)) +
   geom_area(stat = "identity", alpha = 0.7) +
+  geom_label_repel(data = rtt_total_long_waiters_post_recent, aes(label = Count)) + 
   facet_wrap(~Group, scale = "free") +
   scale_fill_manual(values = c("#afc9e9", "#6093d2", "#2d609f", "#163050"), name = "Long Wait Group") +
   scale_x_date(date_breaks = c("4 months"), date_labels = "%b - %y") +
@@ -40,6 +44,24 @@ rtt_total_long_waits_chart_post <- ggplot(rtt_total_long_waiters_post, aes(x = a
   selected_theme(hex_col = "#40C1AC")
 
 rtt_total_long_waits_chart_post
+
+
+rtt_total_long_waits_chart_post_line <- ggplot(rtt_total_long_waiters_post, aes(x = as.Date(Effective_Snapshot_Date), y = Count)) +
+  geom_line(col = palette_tu[4], linewidth = 1.2) +
+  geom_point(data = rtt_total_long_waiters_post_recent, col = palette_tu[4], size = 2) +
+  geom_label_repel(data = rtt_total_long_waiters_post_recent, aes(label = comma(Count)), nudge_x = 90) + 
+  facet_wrap(~Group, scale = "free") +
+  scale_x_date(date_breaks = c("4 months"), date_labels = "%b - %y") +
+  scale_y_continuous(label = comma) +
+  labs(x = "Month Ending",
+       y = "Incomplete Pathways",
+       caption = "Source: Monthly RTT Published Data",
+       title = "Total Incomplete Pathways over 52 weeks",
+       subtitle = "All England - By Waiting Time") +
+  selected_theme(hex_col = "#40C1AC")
+
+rtt_total_long_waits_chart_post_line
+
 
 rtt_total_long_waits_chart_prop <- ggplot(rtt_total_long_waiters, aes(x = as.Date(Effective_Snapshot_Date), y = Prop, col = Group)) +
   geom_line(stat = "identity") +
