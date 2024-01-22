a2a86dc03509a4b7561ac483f28fbd6577521701
ASW-Analyst <andy.wilson8@nhs.net>
2024-01-22 23:02:33 +0000

    Addition of text outputs for key dates and tfc backlog

--- src/outputs/text_outputs.R
+++ src/outputs/text_outputs.R
@@ -1,7 +1,27 @@
 
+# Recent Date -------------------------------------------------------------
+
+recent_date <- format(max(rtt_total_chart_df$Effective_Snapshot_Date), "%B %Y")
+text_min_date <- min(rtt_total$Effective_Snapshot_Date)
+text_max_date <- max(rtt_total$Effective_Snapshot_Date)
+
 # RTT Total Backlog -------------------------------------------------------
 
 rtt_total_text <- rtt_total_chart_df %>%
   filter(Effective_Snapshot_Date == min(Effective_Snapshot_Date)| Effective_Snapshot_Date == "2020-02-29" | Effective_Snapshot_Date == max(Effective_Snapshot_Date)) %>%
   mutate(Incomplete_Pathways_Char = format(Incomplete_Pathways, big.mark = ",")) %>%
   mutate(Incomplete_Pathways_Mil = round(Incomplete_Pathways/1000000, 1))
+
+
+# TFC Total Backlog -------------------------------------------------------
+
+rtt_total_text_tfc <- rtt_tfc_total_chart_df %>%
+  mutate(Effective_Snapshot_Date = as.Date(Effective_Snapshot_Date)) %>%
+  filter(Effective_Snapshot_Date == text_min_date| Effective_Snapshot_Date == "2020-02-29" | Effective_Snapshot_Date == text_min_date) %>%
+  mutate(Incomplete_Pathways_Char = format(Incomplete_Pathways, big.mark = ",")) %>%
+  mutate(Incomplete_Pathways_Mil = round(Incomplete_Pathways/1000000, 1))
+
+tfc_text_perc_1 <- paste0(round(rtt_tfc_total_comp[[1,4]]*100,1), "%")
+tfc_text_perc_2 <- paste0(round(rtt_tfc_total_comp[[2,4]]*100,1), "%")
+tfc_text_1 <- substring(paste0(rtt_tfc_total_comp[[1,1]]), 6, nchar(paste0(rtt_tfc_total_comp[[1,1]])))
+tfc_text_2 <- substring(paste0(rtt_tfc_total_comp[[2,1]]), 6, nchar(paste0(rtt_tfc_total_comp[[2,1]])))
