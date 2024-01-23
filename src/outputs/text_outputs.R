c0de8ec70c7f0ee2a009e2f97f971e0f8fcae25f
ASW-Analyst <andy.wilson8@nhs.net>
2024-01-23 09:16:17 +0000

    Add text values for long waiters

--- src/outputs/text_outputs.R
+++ src/outputs/text_outputs.R
@@ -25,3 +25,20 @@ tfc_text_perc_1 <- paste0(round(rtt_tfc_total_comp[[1,4]]*100,1), "%")
 tfc_text_perc_2 <- paste0(round(rtt_tfc_total_comp[[2,4]]*100,1), "%")
 tfc_text_1 <- substring(paste0(rtt_tfc_total_comp[[1,1]]), 6, nchar(paste0(rtt_tfc_total_comp[[1,1]])))
 tfc_text_2 <- substring(paste0(rtt_tfc_total_comp[[2,1]]), 6, nchar(paste0(rtt_tfc_total_comp[[2,1]])))
+
+
+# Long Waits Groups -------------------------------------------------------
+
+rtt_long_waiters_text <- rtt_total_long_waiters_post %>%
+  filter(Effective_Snapshot_Date == as.Date("2021-06-30") | Effective_Snapshot_Date == text_max_date) %>%
+  mutate(Incomplete_Pathways_Char = format(Count, big.mark = ","))
+
+max_waiters_text <- rtt_total_long_waiters_post %>%
+  group_by(Group) %>%
+  summarise(max = max(Count)) %>%
+  mutate(Incomplete_Pathways_Char = format(max, big.mark = ","))
+
+max_52_waiters_text <- rtt_total_long_waiters_post %>%
+  filter(Group == "52+") %>%
+  filter(Count == max(Count)) %>%
+  mutate(max_date = format(Effective_Snapshot_Date, "%B %Y"))
