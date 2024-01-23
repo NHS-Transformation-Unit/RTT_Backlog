bda479b1e3fcd3f2bd76af049b5528fa5ab92610
ASW-Analyst <andy.wilson8@nhs.net>
2024-01-23 17:24:21 +0000

    Amend ggplot themes for rendering

--- src/config/tu_ggplot_themes.R
+++ src/config/tu_ggplot_themes.R
@@ -43,14 +43,35 @@ theme_tu_white_mf <- function(hex_col, hex_text = "#ffffff") {
   theme(text = element_text(family = "Franklin Gothic Book"),
         strip.background = element_rect(fill = hex_col),
         strip.text = element_text(colour = hex_text, size = 7),
-        axis.text = element_text(size = 9),
+        axis.text = element_text(size = 8),
         axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.title = element_text(size = 8),
+        axis.title = element_text(size = 9),
         plot.title = element_text(size = 16, color = hex_col),
         plot.subtitle = element_text(size = 12),
         panel.background = element_rect(fill = "#ffffff"),
-        panel.grid.major.y = element_line(color = "#cecece", linewidth = 0.1),
-        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x = element_line(color = "#cecece", linewidth = 0.1),
+        panel.grid.minor.x = element_blank(),
+        axis.line = element_line(color = "#000000"),
+        legend.position = "bottom",
+        legend.text = element_text(size = 8)
+  )
+  
+}
+
+# TU Theme White Background multiple facets-----------------------------------------------
+
+theme_tu_white_bar_cf <- function(hex_col, hex_text = "#ffffff") {
+  theme(text = element_text(family = "Franklin Gothic Book"),
+        strip.background = element_rect(fill = hex_col),
+        strip.text = element_text(colour = hex_text, size = 10),
+        axis.text = element_text(size = 10),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.title = element_text(size = 11),
+        plot.title = element_text(size = 16, color = hex_col),
+        plot.subtitle = element_text(size = 12),
+        panel.background = element_rect(fill = "#ffffff"),
+        panel.grid.major.x = element_line(color = "#cecece", linewidth = 0.1),
+        panel.grid.minor.x = element_blank(),
         axis.line = element_line(color = "#000000"),
         legend.position = "bottom",
         legend.text = element_text(size = 8)
