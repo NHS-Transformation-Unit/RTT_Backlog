3a4982754d55de8fc61f2f1e9af322e8015d7a31
ASW-Analyst <andy.wilson8@nhs.net>
2024-01-25 18:27:50 +0000

    Add theme for horziontal bar chart

--- src/config/tu_ggplot_themes.R
+++ src/config/tu_ggplot_themes.R
@@ -100,6 +100,27 @@ theme_tu_white_lf <- function(hex_col, hex_text = "#ffffff") {
   
 }
 
+# TU Theme White Background horizontal bar chart -----------------------------------------------
+
+theme_tu_white_hbar <- function(hex_col, hex_text = "#ffffff") {
+  theme(text = element_text(family = "Franklin Gothic Book"),
+        strip.background = element_rect(fill = hex_col),
+        strip.text = element_text(colour = hex_text, size = 10),
+        axis.text = element_text(size = 10),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.title = element_text(size = 11),
+        plot.title = element_text(size = 16, color = hex_col),
+        plot.subtitle = element_text(size = 12),
+        panel.background = element_rect(fill = "#ffffff"),
+        panel.grid.major.x = element_line(color = "#cecece", linewidth = 0.1),
+        panel.grid.minor.x = element_blank(),
+        axis.line = element_line(color = "#000000"),
+        legend.position = "bottom",
+        legend.text = element_text(size = 8)
+  )
+  
+}
+
 # selected_theme ----------------------------------------------------------
 
 selected_theme <- theme_tu_white
